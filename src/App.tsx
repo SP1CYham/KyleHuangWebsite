76a238686aef184f39e7b75af7c0910ce5124937
KyleHuang <kylehuang1107@gmail.com>
2026-06-17 17:18:22 -0400

    initial web
    just the template

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ padding: "25px" }}>
+        <h1>SPICYHAM</h1>
+        its me
+      </div>
+    </>
+  );
+}
+
+export default App;
