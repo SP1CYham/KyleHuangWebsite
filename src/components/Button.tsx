233e439095a75457745a2a5f1a9791a0803cac73
KyleHuang <kylehuang1107@gmail.com>
2026-06-24 22:54:58 -0400

    installed tailwind and am experimenting
    oh boy

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({
   add = 0,
   setZero = false,
 }: ButtonSettings) {
-  var [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   const handleClick = (e: BaseSyntheticEvent) => {
     setZero ? setCount(0) : setCount(count + add);
@@ -20,7 +20,16 @@ export default function Button({
 
   return (
     <>
-      <button onClick={(e) => handleClick(e)}>{text}</button>
+      <button
+        className="mx-auto flex max-w-sm items-center transition duration-50 ease-in hover:scale-110
+         hover:bg-cyan-900 ring-3 active:bg-cyan-300 active:ring-0 active:shadow-cyan-300 ring-cyan-950 gap-x-4 
+         rounded-xl 
+         bg-white p-6 shadow-cyan-300 shadow-xl/30 text-white outline
+          outline-black/5 dark:bg-slate-800 dark:-outline-offset-1 dark:outline-white/10"
+        onClick={(e) => handleClick(e)}
+      >
+        {text}
+      </button>
       <p>count is {count}</p>
     </>
   );
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,3 @@
-import Button from "./Button";
 import { useState, useEffect } from "react";
 
 export default function Counter() {
